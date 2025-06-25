899a6f64138351671c353aea823a7d8d59a38f1c
LCIMS2 <KursTech@gmail.com>
2025-06-25 15:05:47 +0300

    len() Function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ('Вне условий')
+# Iterable object
+# len() - сколько элементов в объекте
+a = 123456
+length = len(str (a))
+
+print (length)
+
+word = input ('Введите слово для анализа длины:')
+if not word or len(word) < 4:
+        print('Вы ничего не ввели')
+else:
+    print ('Длина слова "' + word + ' " =', len(word))
