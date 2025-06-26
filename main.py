e2e055014b1503d2deaaad604becaad31828c70d
LCIMS2 <KursTech@gmail.com>
2025-06-26 14:55:22 +0300

    for loop begin

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-counter = 1
-word ='поток'
+# цикл
+#for <...> in...:
+# команды
 
-if 'ток' in word:
-    print('Есть')
+#word =  'поток'
+
+#for ch in word:
+#  print(ch)
+#                  0   3   1
+#итератор range(start, stop, step)
+
+for i in range(1, 101): 
+    if i % 10==5:
+        if i == 15:
+            continue
+        print(i)
