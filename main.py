211d510df74e954f3ec5371b59265df23b56c146
LCIMS2 <KursTech@gmail.com>
2025-06-27 15:43:44 +0300

    Encrypting 1

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 # Строки (immutable, iterable)
-# Задача: исправить букву в слове собака
-s = 'сабака'
-res =''
+# Таблица символов
+#s = '\xB0'
+# = '\u2603'
 
-for i in range(len(s)):
-    if i == 1:
-        res += 'о'
-    else:
-        res += s[i]
 
-    print(res)
+#две удобные функции
+# ord(символ) - возвращает код символа в Unicode
+# chr(код) - возвращает код символа по Unicode-коду
+
+#print(u)
+#print('25' + s + 'С')
+#print('Код снеговика в Unicode: {ord('')}')
+#print(chr(10000))
+#print(chr(176)) #ASCII и Unicode
+
+
+s = set()
+word = input ('Введите фразу для зашифровки: ')
+
+for ch in word:
+    s.add(ord(ch))
+
+print(s)
+
+res = ''
+for i in s:
+    res += chr(i)
+
+print(res)
