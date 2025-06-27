71ee0559ce82909eed6ec34362e51251ebd22979
LCIMS2 <KursTech@gmail.com>
2025-06-27 16:13:37 +0300

    Str Repeat Letter

--- main.py
+++ main.py
@@ -1,43 +1,4 @@
-# Строки (immutable, iterable)
-# Таблица символов
-#s = '\xB0'
-# = '\u2603'
+word = 'статор'
 
-
-#две удобные функции
-# ord(символ) - возвращает код символа в Unicode
-# chr(код) - возвращает код символа по Unicode-коду
-
-#print(u)
-#print('25' + s + 'С')
-#print('Код снеговика в Unicode: {ord('')}')
-#print(chr(10000))
-#print(chr(176)) #ASCII и Unicode
-
-
-#s = set()
-#word = input ('Введите фразу для зашифровки: ')
-
-#for ch in word:
-   # s.add(ord(ch))
-
-#print(s)
-
-#res = ''
-#for i in s:
-   # res += chr(i)
-
-#print(res)
-
-
-
-#abc = 'абвгдеёжзийклмнопрстуфхцшщъыьэюя'
-
-phrase = 'Язык Phyton'
-print(phrase.lower())
-print(phrase.upper())
-print(phrase.capitalize())
-print(phrase.title())
-print('Ура!' * 3)
-print ('Телевизор'.count('е'))
-print ('Python'.count('h'))
+for ch in range(1, len(word) + 1):
+    print(word[i - i] * i, end ='')
