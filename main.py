cb6cf478197cbfdb3c8e6667b224ec3a72cf8bbb
LCIMS2 <KursTech@gmail.com>
2025-06-27 14:55:44 +0300

    Change character

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 # Строки (immutable, iterable)
-    #012345
-#s = 'Python'
-# s[3] = 'y' error (immutable)
-# Индекс может быть отрицательным (с конца)
-#print ('Длина слова: {len(s)}')
-#print(s[-1])
+# Задача: исправить букву в слове собака
+s = 'сабака'
+res =''
 
+for i in range(len(s)):
+    if i == 1:
+        res += 'о'
+    else:
+        res += s[i]
 
-s ='язык питон'
-ch = {'я', 'з', 'ы', 'к', 'п', 'и', 'т', 'о', 'н'}
-v = 0 #число гласных
-
-for ch in s:
-    if ch in {'а','е','ё' 'и', 'о', 'у', 'э', 'ю', 'я', 'ы','y', 'o'}:
-        if ch in 'аеёиоуыэюяyo':
-            v += 1
-print(f'Число гласных в слове "{s}" = {v}')
-for index in range (len(s)):
-    print(s[index])
-    print (ch)
-
-
-
-
-
+    print(res)
