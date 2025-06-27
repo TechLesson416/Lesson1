a5d6efd4e88c9463ce369b0738c672dd2c588e75
LCIMS2 <KursTech@gmail.com>
2025-06-27 12:42:32 +0300

    Set Operations

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-# Города
-s = set()
+# PEP8 - правила именования
+# c, l, O, I
+# Операции над множествами
+a = {3, 5, 7}
+b = {3, 5, 7, 9, 11}
 
-while (city := input ('Назовите город: ')) != '':
-    if city in s:
-         print('Такой город уже был')
-else:
-    s.add(city)
-print (f'Итого было названо: {len(s)} городов:')
-for item in s:
-    print('\t', item)
+# Объединение множеств
+c = a.union(b)
+# c = a / b
+print(c)
+
+# Пересечения
+c = a.intersection(b) # и там, и там
+# c = a & b
+print(c)
+
+# разность множеств
+c = a.symmetric_difference(b) # есть в первом, но нет во втором
+# c = b + a
+print(c)
+
+#  разность множеств
+c = b.symmetric_difference(a) # есть в первом, но нет во втором
+# c = b - a
+print(c)
+
+# симметричная разность
+c = a.symmetric_difference(b) # есть в первом, но нет во втором
+# c = b ^ a
+print(c)
