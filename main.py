522cfdf613f2034dfc76dd84f367c7e769e09c42
LCIMS2 <KursTech@gmail.com>
2025-06-27 11:28:27 +0300

    Set Intro

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-#  John
-print ('HELLO GIT!')
-print ('HELLO PYCHARM')
+# коллекции (set, list, dict, tuple)
+# Множества
+# print (dir(s))
+s = set() # пустое множество
+s = {'3', '5', '7', '3', '3', 3}
+s.add('8,5') # добавлени
+s.remove ('3') # вызывает ошибку, если нет
+s.discard ('3') # удаляет вслепую
+s.clear() # очищает множество
+temp = s.pop(5) # даляет случайный и возвращает его
+print (temp)
+print (dir(s))
+print(type (s)) # класс
+print (f'Число элементов в s = {len(s)}')
+print ('Присутствуюет ли 3')
+if '3' in s:
+    print('Да')
+else:
+    print ('Нет')
+for item in s:
+    print (item)
+
+if s:
+
+
+
+
+
+
+
+
