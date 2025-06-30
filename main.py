1a6076c9851df21e41581c2eca08761b38f50dac
LCIMS2 <KursTech@gmail.com>
2025-06-30 14:24:34 +0300

    Ingredients list

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 # Списки (list)
 
-a = ['a','b','c']
-b = a[:] #a.copy()
-b.append ('d') # b += ['d']
-print(id(a))
-print(id(b))
-print (a)
-print (b)
+#a = ['a','b','c']
+#b = a[:] #a.copy()
+#b.append ('d') # b += ['d']
+#print(id(a))
+#print(id(b))
+#print (a)
+#print (b)
+
+
+lst = [] # пустой список
+while (item := input('Индегриент: ')) != '':
+    lst.append(item)
+
+
+
+temp = set(lst)
+lst = list(temp)
+
+print(f'У нас есть {len(lst)} ингредиентов')
+lst.sort()
+
+for i in range (len(lst)):
+    print(f'\t{i + 1}. {lst[i]}')
+
+
+a = ['a', 'b', 'c']
+b = a[:]
+b.append ()
