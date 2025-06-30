e5e3e4d1263d09e324bd6ab1fee52bd352a20b28
LCIMS2 <KursTech@gmail.com>
2025-06-30 10:40:48 +0300

    Start and End of String

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
-#Строки
-#Создаём алфавит
+#Строки (immutable, iterable)
+#Начало и окончание строки
+#startswitch и endswith
 
-alphabet ='абвгджзеёклмнопрстуфчшщъыьэюя'
-#alphabet += alphabet.upper()
-#alphabet_u = alphabet.upper()
+s = 'смотреть'
+s [-1]
 
-#Получаем входные данные
-message = input('Введите строку: ').strip().lower()
-key = int(input('Введите ключ: '))
-
-
-#Инициализируем пустую строку для результата
-encrypted = ''
+if s.startswith('смо'):
+    print('Да')
 
 
-#Перебираем каждый символ в сообщении
-for letter in message:
-    #Проверяем, является ли символ буквой из алфавитва
-    if letter in alphabet:
-        #Находим позицию буквы в алфавите
-        t = alphabet.index(Letter)
-        #Вычисляем новую позицию с учётом сдвига
-        new_key = (t + key) % len (alphabet)
-        #Добавляем зашифрованный символ
-        encrypted += alphabet[new_key]
-    else:
-        #Если символ не буква, оставляем его без изменений
-        encrypted += letter
-
-    # Для расшифровки достаточно изменить формулу вычисления позиции:
-    # new_key = (t - key) % len(alphabet)
-
-    print('Зашифрованное сообщение: ', encrypted)
+if s.endswith('есь'):
+    print('Да')
