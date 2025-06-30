85c98a63ed472d4e669fa3ce60f3fe3d61deddb9
LCIMS2 <KursTech@gmail.com>
2025-06-30 14:33:45 +0300

    pre-Stack

--- main.py
+++ main.py
@@ -7,24 +7,33 @@
 #print(id(b))
 #print (a)
 #print (b)
+#lst = [] # пустой список
+#while (item := input('Индегриент: ')) != '': #выбор ингредиентов
+    #lst.append(item)
+
+#temp = set(lst) #исключение повторов
+#lst = list(temp)
+
+#print(f'У нас есть {len(lst)} ингредиентов: ')
+#lst.sort()
+
+#for i in range (len(lst)):
+    #print(f'\t{i + 1}. {lst[i]}') #упорядовачение вывода списка
+
+#a = ['a', 'b', 'c']
+#b = a[:]
+#b.append ()
+
+# имитация стека
 
 
-lst = [] # пустой список
-while (item := input('Индегриент: ')) != '':
-    lst.append(item)
 
 
 
-temp = set(lst)
-lst = list(temp)
-
-print(f'У нас есть {len(lst)} ингредиентов')
-lst.sort()
-
-for i in range (len(lst)):
-    print(f'\t{i + 1}. {lst[i]}')
 
 
-a = ['a', 'b', 'c']
-b = a[:]
-b.append ()
+
+
+
+
+
