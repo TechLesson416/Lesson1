1aa1dda5d87b7ab793961953ddbf05a1a8a76d78
LCIMS2 <KursTech@gmail.com>
2025-06-30 16:16:57 +0300

    Enumerate

--- main.py
+++ main.py
@@ -1,37 +1,11 @@
 # Кортеж  (tuple, immutable)
 # Студент и средний балл
+# Функция enumerate() - в цикле for возвращает пару (i, v)
 
-#N = 3
-#student = []
+fio = ['Крутов', 'Митрофанова', 'Селезнёв']
 
-#for _ in range(N):
-    #student, average = input('ФИО: '), float(input('Ср. балл: '))
-    #student.append((student, average))
-
-#print(student)
-
-#for st in students:
-   # student, average = st
-   # print('Студент: ', student)
-   # print('Средний балл: ', average)
-
-
-# Функция sorted() - возвращает сортированный список
-
-s = {'Крутов','Селезнёв', 'Митрофанова'}
-r = True
-
-
-lst = sorted(s, reverse=r)
-
-print(*lst, sep=',')
-
-
-#lst = List(s)
-#lst.sort()
-
-#print(*lst, sep=',')
-
+for i, v in enumerate(fio):
+    print(f'{i + 1}. {v}.')
 
 
 
