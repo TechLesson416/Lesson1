22d2e1c322743cf1239ffc4735ba7d9f82616b08
LCIMS2 <KursTech@gmail.com>
2025-06-30 14:57:06 +0300

    Abbreviation

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 # Списки (list)
-# Имитация стека
+# Создание аббревиатур
 
+lst = []
 
-N = 5
+while (word := input ('Введите слово: ')).strip().capitalize != '':
+    lst.append(word[0].upper())
 
-lst = [] #Пустой список
+print('Получилась аббревиатура', end=': ')
+print(*lst[:10], sep='')
 
-for i in range(N):
-    print(f'Кладём книгу {i + 1} в стопку.')
-    lst.append(i + 1)
-
-print ('---')
-
-while lst:
-    item = lst.pop(0) # стек стоит по умолчанию
-    print (f'Берём книгу {item} из стопки.')
 
 
 
