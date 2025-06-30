218223daf361556aad8331589a9a3c59af3f0d76
LCIMS2 <KursTech@gmail.com>
2025-06-30 11:13:33 +0300

    Find and
    it`s overloading

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 #Строки (immutable, iterable)
 #Начало и окончание строки
 #startswitch и endswith
+# 1. find('подстрока', start)
+# 2. find('подстрока', start) - с какого места искать
 
-s = 'смотреть'
-s [-1]
+s = 'сихрофазотрон' #ищем 'о': сколько их и где находятся
+ch = 'о'
 
-if s.startswith('смо'):
-    print('Да')
+numbers = set()
 
-
-if s.endswith('есь'):
-    print('Да')
+if ch in s:
+    count = s.count(ch)
+    print(f'Буква {ch} встечается в слове {s} {count} раз.')
+    print('Её позиция/позиции:', end=' ')
+    start = 0
+    for i in range(count):
+        pos = s.find(ch, start)
+        numbers.add(pos)
+        start += pos
+        print(pos, end=' ')
+else:
+    print(f'Буквы \'{ch}\' нет в слове "{s}".')
