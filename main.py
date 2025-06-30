b2565c30d68e60b27ddf571dd58385161a972f7e
LCIMS2 <KursTech@gmail.com>
2025-06-30 12:53:31 +0300

    List intro

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-#Строки (immutable, iterable)
-#Срез (у строки и у других коллекций, кроме set)
-#[начало:окончание:шаг]
+# Списки (list)
 
-#s = input('введите строку :').strip() # 'потоп'
-#if s == s[::-1]:
-  #  print(f'Строка "{s}" - палиндром!')
-#else:
-   # print(f'Строка "{s}" - не палиндром!')
-#a = 'Python'
-
-# Город Миргород
-s = 'Дорог Рим'
-
-temp = s.lower()
-city = temp[:5][::-1]
-res = city + '' + temp[6:][::-1] + city
-
-print(res.title())
+# lst = [] # пустой список
+lst = list(range(10))
+slice = lst[2::2]
+print(slice)
+for item in range(0, len(lst), 2):
+    print(lst[item], '-', item ** 2)
 
