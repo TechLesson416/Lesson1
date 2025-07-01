089e128b3fad253d7c71689cb203de58bd7e646a
LCIMS2 <KursTech@gmail.com>
2025-07-01 11:40:56 +0300

    Comprehension task

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-#Списочные выражения (list comprehension)
-#squares = [i**2 for i in range(10)]
-# squares
-#fro i in range(10):
-#   squares.append(i ** 2)
-
-# список квадратов чисел
-# squares = [i ** 2 for i in range(10)]
+# Списочные выражения (list comprehension)
+# Занести в список каждое третье слово из предложения
+text = 'Списочные выражения применяются для эффективности кода'
 
 
-# список квадратов чётных чисел
-squares = [i ** 2 for i in range(10) if i % 2 == 0]
-
-
-print(*squares, sep=',')
-
-# произведение i и j
-print([i * j for i in range(3) for j in range(3)])
-
-for i in  range(3):
-        for j in range(3):
-            print(i * j)
-
-n = '100 200 300 400 500 600 700 800 900'
-approved = ['500', '800']
-a = [int(i) for i in n.split() if int(i) in approvred]
-#какие-то действия со списком а
-print(a)
+#res = [a for a in text.split() if (text.index(a) + 1) % 3]
+res = set(a for a in text.split()[2::3])
+#
+print(res)
