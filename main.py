0305fb798654f6a7f0fa6b6d9fd4554308be1f64
LCIMS2 <KursTech@gmail.com>
2025-07-01 11:13:03 +0300

    List comprehension 1

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+#Списочные выражения (list comprehension)
+#squares = [i**2 for i in range(10)]
+# squares
+#fro i in range(10):
+#   squares.append(i ** 2)
 
-text = '  P  y   t h o n '
-temp = text.split()
-res = ''.join(text.split()) # убрать все пробелы
-print(res)
+# список квадратов чисел
+# squares = [i ** 2 for i in range(10)]
 
 
+# список квадратов чётных чисел
+squares = [i ** 2 for i in range(10) if i % 2 == 0]
+
+
+print(*squares, sep=',')
+
+# произведение i и j
+print([i * j for i in range(3) for j in range(3)])
+
+for i in  range(3):
+        for j in range(3):
+            print(i * j)
+
+n = '100 200 300 400 500 600 700 800 900'
+approved = ['500', '800']
+a = [int(i) for i in n.split() if int(i) in approvred]
+#какие-то действия со списком а
+print(a)
