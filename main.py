f136e5bd78ba22aeca3260607511407603334709
LCIMS2 <KursTech@gmail.com>
2025-07-01 12:18:21 +0300

    Nested lists generation

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-# Списочные выражения (list comprehension)
-# Занести в список каждое третье слово из предложения
-text = 'Списочные выражения применяются для эффективности кода'
+# Вложенные списки
 
+#a = [1, 38.6, True, False, 'sfs', (1,2)]
 
-#res = [a for a in text.split() if (text.index(a) + 1) % 3]
-res = set(a for a in text.split()[2::3])
-#
-print(res)
+#N = 3
+#matrix = [
+ #   [1,2,3],
+ #   [4,5,6],
+#    [7,8,9],
+#]
+
+#matrix = [[1]* N for _ in range(3)]
+#print(matrix)
+
+#Обход двухмерного списка (матрицы)
+#count = 1
+#for row in range(len(matrix)):
+ #   for col in range(len(matrix[row])):
+ #       matrix[row][col] = count
+  #      count += 1
+ #   print(matrix)
+#print(matrix)
+
+N = 4
+
+for i in range(N):
+    for j in range(start, start + N):
+        table.append(j)
+    matrix.append(table)
+    table = []
+    tart += N
+
+print(matrix)
