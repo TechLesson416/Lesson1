688d86ba911cbe75b9876d9246488cb54c02154d
LCIMS2 <KursTech@gmail.com>
2025-07-01 14:25:24 +0300

    Dictionary Methods

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ d = {
     'chair': 'стул',
     'apple': 'яблоко',
     1: 'один',
+    (55.75, 37.5): 'Москва'
 }
+
+print(d[(55.75, 37.5)])
 
 
 print(d['well'][0])
@@ -25,12 +28,25 @@ deleted_item = d.pop('apple')
 print('Удалился элемент:', deleted_item)
 
 print('Есть ли стул в словаре')
-if 'chair' in d:
+if 'стул' in d.values():
     print('Да, есть')
 
+print('Доступ к несуществующему ключу без "Без исключений"')
+pear = d.get('pear', 'Груши нет')
+print('Где груша: ', pear)
 
-for key in d:
-    print(key, '->', d[key])
+
+
+
+
+# Перебор пар "ключ-значение"
+for k, v in d.items(): #d.keys()
+    print(k, '->', v)
+
+print(d.keys()) # список ключей (list)
+print(d.values()) # список значений (list)
+print(d.items()) # список пар (ключ значения
+
 
 #print(d) - словарь целиком "как  есть"
 
