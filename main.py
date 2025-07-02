c1cbbc1d8937c265a187d8bd6cca1993453801fc
LCIMS2 <KursTech@gmail.com>
2025-07-02 15:43:37 +0300

    Args and Kwargs

--- main.py
+++ main.py
@@ -1,45 +1,22 @@
-# Функция, с переиенным числом аргументов
-def multy(*args):
-    # print(len(args)) # подсчёт числа аргументов
-    # print(args) # по индексу, либо перебором в цикле
-    # if len(args) == 0
-        # return 0
-    # result = 1
-    if not args:
-        return 0
-    result = 1
-    for arg in args:
-        result *= arg
-    return result
 
+def print_any(*args, **kwargs):
+    for i in args:
+        print(i)
+    for k, v in kwargs.items():
+        print(k, '=', v)
 
-print(multy(1, 2, 3, 4))
+def profile(name, surname, city, *children, **additional):
+     print(f'Имя:', {name})
+     print(f'Фамилия: {surname}')
+     print(f'Из города: {city}')
+     if len(children) > 0:
+         print('Дети:',','.join(children))
+     if 'hobbie' in additional:
+         print('Хобби:',','.join(additional['hobbie']))
+    #print(additional)
 
-# Применяем is на практике
-# def print_array(array: list, start: int = None):
-#     if start is not None and start > len(array):
-#         return
-#     if start is None:
-#         start = 0
-#         for i in range(start, len(array)):
-            # print(array[i])
-
-# Возврат нескольких значений из функции
-# При распаковке '*" может быть только одна
-def coordinates() -> tuple:
-    return 5.4, 3.2, 3.8, 7.2, 4.6
-
-
-x, y, *rest = coordinates() # распаковка
-print(f'x = {x}, y = {y}, rest = {rest}')
-
-*names, surname = 'Остап Сулейман Бендер'.split()
-print(names, surname)
-
-print(coordinates())
-
-
-
+    profile('Дмитрий', 'Колесов', 'Волгоград',
+    'Мария','Пётр',hobbie='Филателия', 'Шахматы'])
 
 
 
