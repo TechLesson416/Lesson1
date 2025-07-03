22508c75897183a55f937da0ca822b6318c63a44
LCIMS2 <KursTech@gmail.com>
2025-07-03 16:04:06 +0300

    Recursion Factorial

--- main.py
+++ main.py
@@ -1,23 +1,65 @@
-# словарные выражения
-# lambda <аргументы>: <выражение>
-# Анонимные функции (однострочники, безымянные
-# lambda-функции
-#Потоковый ввод sys.stdin (Ctrl + D)
+# Рекурсия - функция вызывает сама себя
+# def factorial(count): #5! = 1 * 2 * 3 * 4 * 5 = 120
+#     result = 1
+#     for i in range(2, count + 1):
+#         result *= i
+#     return result
+#
+# for x in range(10):
+#     print(x, factorial(x))
 
-import sys
-#data = sys.stdin.readlines()
-data = [d.strip('\n') for d in sys.stdin.readlines()]
+# def factorial(x):
+#     if x == 1 or x == 0:
+#         return 1
+#     return x * factorial(x - 1)
+#
+# for x in range(10):
+#     print(x, factorial(x))
 
-temp = [] # индекс строкив data и число слов в виде кортежей
-for i, s in enumerate(data):
-    temp.append((i, len(s.split())))
+# Черепашья графика
 
-temp.sort(key=lambda x:x[1])
+# N  = 5
+#
+import turtle as t
+t.speed(0)
+# colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+#
+# t.bgcolor('black')
+# angle = 360 // len(colors) - 1
+#
+# for x in range(200):
+#     t.pencolor(colors[x % len(colors)])
+#     t.width(x // 100 + 1)
+#     t.forward(x)
+#     t.left(angle)
+#
+# t.penup()
+# t.goto( 100, 200 )
+# t.pendown()
+#
+# def square(side):
+#     for _ in range(4):
+#         t.forward(side)
+#         t.right(90)
+#
+# def flower():
+#     for _ in range(36):
+#         t.circle(50)
+#         t.right(10)
 
-index = temp[0][0]
-res = sorted(data[index].split())
+def tree(length):
+    if length < 10:
+        return
+    t.forward(lenth)
+    t.left(30)
+    tree(length * 0.7)
+    t.right(60)
+    tree(length * 0.7)
+    t.left(30)
+    t.backward(length)
+   # flower()
 
-print(*res, sep='-')
+t.left(90)
+tree(100)
 
-print(data)
-
+t.mainloop()
