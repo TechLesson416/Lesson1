f010fb301ff238a1223b1434f7897e33bfd3843b
LCIMS2 <KursTech@gmail.com>
2025-07-03 12:21:49 +0300

    Sorting Key

--- main.py
+++ main.py
@@ -1,49 +1,23 @@
-#ENGLISH_ABC = [chr(ch) for ch in range(ord('a'),ord('z')+1)]
-#print(ENGLISH_ABC)
-#RUSSIAN_ABC - [chr(ch) for ch in range(ord('а'), ord('я')+1)] + ['ё']
-#print(RUSSIAN_ABC)
 # словарные выражения
 # lambda <аргументы>: <выражение>
+# Анонимные функции (однострочники, безымянные
+# lambda-функции
+#Потоковый ввод sys.stdin (Ctrl + D)
 
-numbers = [1, 2, 3, 4, 5] # list(range 1, 6)
-squares = {n: n ** 2 for n in numbers}
-print(squares)
+import sys
+#data = sys.stdin.readlines()
+data = [d.strip('\n') for d in sys.stdin.readlines()]
 
-numbers = range (1, 11)
-squares = {n: n ** 2 for n in range(1, 10) if n % 2 == 0}
-print (squares)
+temp = [] # индекс строкив data и число слов в виде кортежей
+for i, s in enumerate(data):
+    temp.append((i, len(s.split())))
 
-source_dict = {
-    'x': 1,
-    'y': 2,
-    'z': 3,
-}
+temp.sort(key=lambda x:x[1])
 
-dest_dict = {k: v * 2 for k, v in source_dict.items()}
-print(dest_dict)
+index = temp[0][0]
+res = sorted(data[index].split())
 
+print(*res, sep='-')
 
+print(data)
 
-# ENGLISH_ABC = [chr(ch) for ch in range(ord('a'),ord('z')+1)]
-# RUSSIAN_ABC = [chr(ch) for ch in range(ord('а'),ord('я')+1)] + ['ё']
-# ABC = set(ENGLISH_ABC) ^ set(RUSSIAN_ABC) ^ set([x.upper() for x in ENGLISH_ABC])
-# print(ABC)
-# # print(ENGLISH_ABC)
-# # print(RUSSIAN_ABC)
-# txt = 'Однажды, теперь и потом'.lower()
-#
-#
-# def remove_punctuation(txt):
-#
-#     return ''.join(filter(lambda x: x in ABC ^ {' '}, txt))
-#
-#
-# print(remove_punctuation(txt)(txt))
-#
-# def get_words(txt: str) -> filter:
-#     return remove_punctuation(txt).split()
-#
-# def long_words(txt, length=4) -> filter:
-#     return filter(lambda word: len(word) >= length, get_words(txt))
-#
-# print(long_words(txt))
