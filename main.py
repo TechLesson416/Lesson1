4619b8b4cf1850569289187c09b648bdee42d0f0
LCIMS2 <KursTech@gmail.com>
2025-07-07 16:15:21 +0300

    OS: File list

--- main.py
+++ main.py
@@ -1,109 +1,70 @@
-    # Файлы - набор данных, сохранённых на носителях информации определённой структуры
-    # name.txt
-    # t - текстовый файл (txt, html, xml)
-    # b - бинарные файлы (jpg, avi, mp3)
-    # w - write (запись, создаётся\удаляется)
-    # a - append (запись в конец)
-    # r - read - чтение (по умолчанию)
-    # print(*args, sep='', end='\n', file=None, flush=False)
+# Файлы и ОС-модуль
 
-#     fo = open('info.txt', 'wt', encoding='utf-8')
-#     print(fo.mode)
-#     print(fo.name)
-#     print(fo.encoding)
-#
-#     count = fo.write('Этот текст будет в файле!')
-#     print('В файл записано', count, 'байт!')
-#
-# fo.close()
+import  os
 
-fo = open('info.txt', 'rt', encoding='utf-8')
+path = os.getcwd() # get current working directory
+os.chdir(patr +'/images')
+
+
+all_files = [f for f in os.listdir('.') if f.endswith('.jpg')]
+os.chdir('..')
+print(all_files)
+
+#os.cndir('..') # на уровень выше
+# os.chdir(path + '/fonts')
+# print(os.getcwd())
+#
+#
+#
+#
+#
+# # "Мягкое" создание директорий (вместо mkdirs)
+# #os.makedirs('libs', exist_ok=True)
+#
+# if os.path.exists('libs'): # проверка существования пути
+#     os.rmdir('libs')
+
+
+
+
+# Открытие с менеджером контекста
+# with open('info.txt', 'rt+', encoding='utf-8') as fo:
+#     text = fo.read()
+#     lst = text.splitlines()
+#     print(lst)
+# Проследит, чтобы файл закрылся
 
 #fo.write ('Хороший текст.')
 
 
-text = fo.read(11)
-fo.read(6)
-text += fo.read(7)
-print('\n А Вот, что было в файле', file=fo)
-
-text = fo.readline()
-print(text)
-text = fo.readline()
-print(text)
-
-# Построчное чтение
-while text := fo.readline():
-    print(text.rstrip('\n'))
-
-# Построчное чтение №2
-lst = fo.readlines()
-lst = list(map(lambda x: x.strip('\n'), lst))
-print(lst)
-
-fo.close()
-
-# Построчное чтение № 3
-text = fo.read()
-lst = text.splitlines()
-print(lst)
-
-
-
-
-
-
-# Пишем свои модули
-# from lib import diff
+# text = fo.read(11)
+# fo.read(6)
+# text += fo.read(7)
+# print('\n А Вот, что было в файле', file=fo)
 #
-#     def main():
-#         print (summ(a)
+# text = fo.readline()
+# print(text)
+# text = fo.readline()
+# print(text)
 #
+# # Построчное чтение
+# while text := fo.readline():
+#     print(text.rstrip('\n'))
 #
-#     if __name__== '__main__':
+# # Построчное чтение №2
+# lst = fo.readlines()
+# lst = list(map(lambda x: x.strip('\n'), lst))
+# print(lst)
 #
+# fo.close()
 #
-# print(diff(7, 3))
-#
-# print(__name__)
-
-# from package1 import * # для __
-# import package1
-# from package1 import *
-#
-#
-# print(greet('Мир!'))
-# print(add(3,7))
-# print ('Автор',)
-
-#print(package1.module._hidden_function())
-
+# # Построчное чтение № 3
+# text = fo.read()
+# lst = text.splitlines()
+# print(lst)
 
 
 
-
-
-#Работа с формулами:
-#....
-#ws['A1'] = "=SUM(A1:A10)"
-# Формат:
-# from openlynx import load_workbook
-# from openpyxl.styles import Font, Aligment
-# #....
-# ws['A1'].font = Font(bold=True, size=14)
-# ws['A1'].alignment = Aligment(horizontal="center")
-#
-# # Чтение данных
-# from openlynx import load_workbook
-#
-# wb = load_workbook('docs/employees.xlsx')
-# ws = wb.active
-#
-# rows_count = ws.max_row
-#
-# for row in ws.iter_rows(values_only=True):
-#     fio, pos, dept = row
-#     print(f'Фамилия: {fio}, Должность: {pos}, Отдел: {dent}')
 
 
 
