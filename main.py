94f945c24b3652b16d2aa8dc3a92f37215fe4329
LCIMS2 <KursTech@gmail.com>
2025-07-08 11:29:36 +0300

    Exceptions Intro

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
+# Исключения (runtime)
+# try:
+#   что пытаемся сделать
+# except:
+#   обрабатываем исключения
+# else:
+#   если исключения не было
+# finally:
+#   выполняются в любом случае
+###################################################################
+flag = False # открывался ли на запись
+
+try:
+    fo = open('inforamtion.txt', encoding='utf-8')
+    print(fo.read())
+    fo.close()
+except FileNotFoundError:
+    fo = open('information.txt', 'wt', encoding ='utf-8')
+    flsg = True
+    print('Файл не обнаружен и создан с параметрами по умолчанию')
+    # with open('inforamtion.txt', 'wt', encoding='utf-8') as fo:
+    #     fo.write('По умолчанию')
+else:
+    print('Файл открыт успешно. Читаем его')
+    print(fo.read())
+    fo.close()
+finally:
+    if flag: # если флаг был открыт на запись
+        fo.write('По умолчанию')
+        fo.close()
+    print('Продолжаем работать.')
+###################################################################
 
 
-print(img_dir)
-
-img_dir = path.join(path.dirname(__file__), 'images')
-font_dir = path.join(path.dirname(__file__), 'fonts')
 
 
 
