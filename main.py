c24cd25cc960699174bc65d17ee0b957f66ee630
LCIMS2 <KursTech@gmail.com>
2025-07-08 14:32:36 +0300

    Clever Dump

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
-# Задача 2
+
+    # Практикум (обучаемый словарь)
+import pickle
+
+# минимальная версия, если файл dict.dat отсутствует
+voc = {
+       'стол': 'tables',
+        'стул': 'chair',
+    }
+
+# функция для распечатки словаря
+def print_voc():
+        print('Сейчас словарь содержит: ')
+        for k, v in voc.items():
+            print(k, '-', v)
+
+# загружаем словарь из файла
+try:
+    with open('dict.dat', 'rb') as dump_in:
+        voc = pickle.load(dump_in)
+except FileNotFoundError:
+    with open('dict.dat', 'wb') as dump_out:
+        pickle.dump(voc, dump_out)
+    print('Создан минимальный словарь: ')
+    print_voc()
+
 while True:
-    a = input('Введите первое число: ')
-    b = input('Введите второе число:')
-    try:
-        result = int(a) / int(b)
-    except ZeroDivisionError:
-        print('На ноль делить нельзя')
-    except VGalueError:
-        print('Нужно вводить числа...')
-        print(f'А введено: {a} и {b}:')
-        pass
-    else:
-        print(result)
+    temp = input('\nВведите слово для перевода или "#" для завершения')
+    word = temp.strip().lower()
+    if word == '#' or word == '№':
         break
+    if word in voc.keys():
+        translate = voc[word]
+        print(f'Слово "{word}" переводится как {translate}.\n')
+    else:
+        print(f'Значение слова {word} остутствует в словаре.')
+        newkey = f'А как слово {word} переводится. \n  '
+        newkey += 'Если ничего не вводите нажмите ENTER, \n'
+        newkey += 'или введите его здесь: '
+        new_word = input(newkey)
 
+        if new_word != '' or len(new_word) > 2:
+            voc[word] = new_word
+            print(f'Слово {word} с переводом внесено в словарь')
+        else:
+            print('Ничего не введено или слишком короткое слово')
+            continue
 
+# Сохранить словарь
+with open('dict.dat', 'wb') as dump_out:
+    pickle.dump(voc, dump_out)
 
-    # a = input('введите первое число: ')
-    # b = input('Введите второе число: ')
-    #
-    # if a.isdigit() and b.isdigit():
-    #     if int(b) == 0:
-    #         print('На ноль делить нельзя')
-    #     else:
-    #         print(int(a) / int(b))
-    #         break
-    # else:
-    #     print('Вводить надо только числа.')
+print('До новых встреч!!!')
