75ab0bc358f771a1edbecd04e01669e35845b183
LCIMS2 <KursTech@gmail.com>
2025-07-08 16:27:07 +0300

    Regular Expressions 2

--- main.py
+++ main.py
@@ -1,108 +1,81 @@
+# Регулярные выражения (поиск по паттерну)
+# Regular Expressions (re)
+# r-строка - raw-string ("сырая" строка)
+# Квантитификаторы (quantity)
+# {m} - ровно m раз
+# {m,} - m раз и более
+# {,n} - не более n раз
+# {m, n} - от m до n (без пробела)
+# ? - от нуля до одного (аналог {0,1})
+# * - от нуля до бесконечности (32767) {0,}
+# + - от одного до бесконечности {1,}
+import re
+
+# pattern = r'\b\w{4}\b' # все слова из 4 символов
+# pattern = r'\d' # все цифры от 0 до 9
+# pattern = r'начало!\Z' # На что заканчивается
+# pattern = r'[0-5][0-9]' # Две идущие подряд
+# pattern = r'[а-яА-я]' # все буквы от а до я и от А до Я
+# pattern = '[^ерм]' # исключить символы
+# pattern = r'o{2,5}' # вытащить текст из скобок
+pattern = 'Go{2,5}gle' # Google
+test_string = 'Google' , 'Gooogle', 'Gooooooogle'
+
+result = re.findall(pattern, test_string)
+print(result)
+# Thernary If (тернарный условный оператор)
+print('Цифры есть') if result else print('Цифр нет')
+
+
+
+
+
+
+
+
+
+
+
+# Линтерны - контролирует следование хорошим практикам
+# Flake 8
+# pip install flake8
+# (flake8-bugbear - для нахождения  логических ошибок в коде)
+# (pep8-naming - проверяет имена на соответствие pep8)
+# pip install flake8-bugbear pep8-naming
+# Arguments --max-complexity 10 $FileDir$/$FileName$
+# Path: $FileDir$
+# Advanced Options/OutputFilter: #FILE_PATH$:$LINES$
+
+
+
+
+
+
 # Библиотека pymorphy
 # pip install pymorphy3
 # pip install -U pymorphy3-dicts-ru
-from pymorphy3 import MorphAnalyzer
-
-form = MorphAnalyzer().parse('бутылка')[0]
-
-for btl in reversed(range(99)):
-    print(f'В холодильнике {btl + 1} {form.make_agree_with_number(btl + 1).word} пива')
-    print('Возьмёмодну и выпьем')
-    if btl % 10 == 1 and btl != 11:
-        remain = 'Осталась'
-    else:
-        remain = 'Осталась'
-    print(f'{remain} {btl} {form.make_agree_with_number(btl).word} пива.')
-
-
-
-import pymorphy3
-
-morph = pymorphy3.MorphAnalyzer()
-
-print(morph.parse('пила'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # Практикум (обучаемый словарь)
-# import pickle
+# from pymorphy3 import MorphAnalyzer
 #
-# # минимальная версия, если файл dict.dat отсутствует
-# voc = {
-#        'стол': 'tables',
-#         'стул': 'chair',
-#     }
+# form = MorphAnalyzer().parse('бутылка')[0]
 #
-# # функция для распечатки словаря
-# def print_voc():
-#         print('Сейчас словарь содержит: ')
-#         for k, v in voc.items():
-#             print(k, '-', v)
-#
-# # загружаем словарь из файла
-# try:
-#     with open('dict.dat', 'rb') as dump_in:
-#         voc = pickle.load(dump_in)
-# except FileNotFoundError:
-#     with open('dict.dat', 'wb') as dump_out:
-#         pickle.dump(voc, dump_out)
-#     print('Создан минимальный словарь: ')
-#     print_voc()
-#
-# while True:
-#     temp = input('\nВведите слово для перевода или "#" для завершения')
-#     word = temp.strip().lower()
-#     if word == '#' or word == '№':
-#         break
-#     if word in voc.keys():
-#         translate = voc[word]
-#         print(f'Слово "{word}" переводится как {translate}.\n')
+# for btl in reversed(range(99)):
+#     print(f'В холодильнике {btl + 1} {form.make_agree_with_number(btl + 1).word} пива')
+#     print('Возьмёмодну и выпьем')
+#     if btl % 10 == 1 and btl != 11:
+#         remain = 'Осталась'
 #     else:
-#         print(f'Значение слова {word} остутствует в словаре.')
-#         newkey = f'А как слово {word} переводится. \n  '
-#         newkey += 'Если ничего не вводите нажмите ENTER, \n'
-#         newkey += 'или введите его здесь: '
-#         new_word = input(newkey)
+#         remain = 'Осталась'
+#     print(f'{remain} {btl} {form.make_agree_with_number(btl).word} пива.')
 #
-#         if new_word != '' or len(new_word) > 2:
-#             voc[word] = new_word
-#             print(f'Слово {word} с переводом внесено в словарь')
-#         else:
-#             print('Ничего не введено или слишком короткое слово')
-#             continue
 #
-# # Сохранить словарь
-# with open('dict.dat', 'wb') as dump_out:
-#     pickle.dump(voc, dump_out)
 #
-# print('До новых встреч!!!')
+# import pymorphy3
+#
+# morph = pymorphy3.MorphAnalyzer()
+#
+# print(morph.parse('пила'))
+#
+#
+#
 
 
