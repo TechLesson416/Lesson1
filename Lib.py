b061af021ffd7d5684121f859090793dbeb300f3
LCIMS2 <KursTech@gmail.com>
2025-07-10 15:08:35 +0300

    Special (Magic) Methods Call

--- Lib.py
+++ Lib.py
@@ -1,3 +1,6 @@
+from multiprocessing.util import sub_debug
+
+
 class Book:
     def __init__(self, title, author):
         self._title = title
@@ -15,3 +18,11 @@ class Student:
     def __init__(self, name='Bill', univ=''):
         self.name = name
         self._company = univ
+
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        super.__init__(side, side)
+        self.side = side
+        self.name = 'квадрат'
--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-# ООП (magic methods)
-# method override; operator overloading
-
-#
-# class myTime:
-#     def __init__(self, minutes, seconds):
-#         if 0 <= minutes < 60:
-#             self.minutes = minutes
-#         if 0 <= seconds < 60:
-#             self.seconds = seconds
-#
-#     def __add__(self, other):
-#         m = self.minutes + self.minutes
-#         s = self.seconds + other.seconds
-#         m += s // 60
-#         s = s % 60
-#         m = m % 60
-#         print(m, s)
-#         return myTime(m, s)
-#
-#     def __str__(self):
-#         return f'<Time {self.minutes:02}:{self.seconds}>'
-#
-# t1 = myTime(13, 0)
-# t2 = myTime(53, 5)
-# print(t1 + t2)
-
+# ООП (inheritance)
+# Класс, от которого наследуем: базовый, родительский, суперкласс
+# Класс, который наследуется: производный, дочерний
+from Lib import Rectangle
 
 
 # __call__ - экземпляр класса становится вызываемым
 # (как функция)
 # y = ax^2 + bx + c
+
 
 class SquareFunction:
     def __init__(self, a, b, c):
@@ -43,4 +21,21 @@ class SquareFunction:
 
 s = SquareFunction(1, 2, 3 )
 print(s(2))
+
+class Square(Rectangle):
+    def __init__(self, width, height):
+        super().__init__(side, side)
+        self.side = side
+        self.name = 'квадрат'
+
+    # def perimeter(self):
+    #     return self.name
+
+
+
+s = Square(5)
+print(s.area())
+print(s.perimeter())
+print(s.get_name())
+
 
