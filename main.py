134556db28b914dcd85f3b25dfe5e06ccfca08ba
LCIMS2 <KursTech@gmail.com>
2025-07-10 16:13:26 +0300

    Inheritance Intro

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
-# ООП (inheritance)
-# Класс, от которого наследуем: базовый, родительский, суперкласс
-# Класс, который наследуется: производный, дочерний
-from Lib import Rectangle
+#ООП Class
 
 
-# __call__ - экземпляр класса становится вызываемым
-# (как функция)
-# y = ax^2 + bx + c
+BankAccount
+
+owner_name
+
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
 
 
-class SquareFunction:
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+    def get_balance(self):
+        return self._balance
 
-    def __call__(self, x):
-        return self.a * x ** 2 + self.b * x + self.c
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+            print(f'Декпозит пополнен на сумму {amount}')
+        else:
+            print(f'Нельзя вносить отрицательную сумму на депозит.')
+
+    def withdraw(self, amount):
+        if 0 < amount < self._balance:
+            self._balance -= amount
+            print(f'С депозита снята сумма {amount}.')
+        else:
+            print(f'Не хвататет средств. Овердрафт не доступен.')
+
+client = BankAccount('John')
+client.deposit(500)
+client.withdraw(600)
+print('Остаток:', client1.get_balance())
 
 
-s = SquareFunction(1, 2, 3 )
-print(s(2))
-
-class Square(Rectangle):
-    def __init__(self, width, height):
-        super().__init__(side, side)
-        self.side = side
-        self.name = 'квадрат'
-
-    # def perimeter(self):
-    #     return self.name
 
 
 
-s = Square(5)
-print(s.area())
-print(s.perimeter())
-print(s.get_name())
+
+
+
+
+
+
+
+
+
 
 
