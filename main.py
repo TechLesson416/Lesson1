6935d7abfaa73ad31529a91a4fb8e799c39b6dce
LCIMS2 <KursTech@gmail.com>
2025-07-11 12:29:47 +0300

    Periodical Task

--- main.py
+++ main.py
@@ -1,37 +1,68 @@
-#ООП Class
+# Протокол - Совокупность правил, которые регламентируют функции передачи данных между компонентами компьютерных сетей
+
+# Протоколы
+# (TCP) - Transmission Control Protocol (Протокол управления передачей)
+# Internet Protocol (IP) - разбивает на пакеты (IP-дейтаграммы), определяет их направление
+# TCP/IP
+# HTTP(S) - Hyper Text Trans Protocol (Формат HTML) (S - Secured)
+# http://www. - Браузерный протокол
+# FTP - File Transfare Protocol
+# SMTP - Simple Mail Transfer Protocol
+# Хост-системы - Компьютер, являющийся частью системы, к которой подключаются другие компьютеры
+# Кодировки Хост-системы:
+# 1. Обязательная (Дружественная компьютеру, но не человеку) - IP-адрес: 195.34.32.11
+# 2. Необязательная (Наоборот^) - DNS (Domain Name System)
+# http(s)://www.yandex.ru//
+# ASCII %20
+# URL - Uniform Resource Locator
+# whois.
+# nic.ru
+# reg.ru
+# http(s)://домен.зона/page1/?param1=value1&param2=value2
 
 
-BankAccount
+# Командная строка
+# dir - листать содержание каталога и прочее
+# ../images/
+#rm - (remove) r templates - переход в директорию templates и удаление файлов без возможности восстановления
+#cp (copy) - источник, копирование файла вдругую директорию
+#mk dir - создать директорию
+#cd - change directory
+#mv - move
+# import sys
+#
+# print ('Я', sys.argv[0], 'и мой аргумент', sys.argv[1])
+#
+#
+# if len(sys.argv) >= 2:
+#     match sys.argv[1]:
+#         case 'p':
+#             print('Привет')
+#         case 'g':
+#             print('Пока')
+#         case _:
+#             print('Не понял')
 
-owner_name
 
-class BankAccount:
-    def __init__(self, owner, balance=0):
-        self.owner = owner
-        self.balance = balance
+# Периодические задачи
+import schedule
+import datetime
+
+i = 1
+
+def job():
+    global i
+    print(f'Скрипт запустился {i}-раз')
+    i += 1
+    t = datetime.datetime.now()
+    print('Время:', t.strftime('%H:%M:%S'))
 
 
-    def get_balance(self):
-        return self._balance
+schedule.every(3).seconds.do(job)
 
-    def deposit(self, amount):
-        if amount > 0:
-            self._balance += amount
-            print(f'Декпозит пополнен на сумму {amount}')
-        else:
-            print(f'Нельзя вносить отрицательную сумму на депозит.')
+while True:
+    schedule.run_pending()
 
-    def withdraw(self, amount):
-        if 0 < amount < self._balance:
-            self._balance -= amount
-            print(f'С депозита снята сумма {amount}.')
-        else:
-            print(f'Не хвататет средств. Овердрафт не доступен.')
-
-client = BankAccount('John')
-client.deposit(500)
-client.withdraw(600)
-print('Остаток:', client1.get_balance())
 
 
 
