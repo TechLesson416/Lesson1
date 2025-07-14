9b04f0bd49623ee3b70a5284f31e1e858ab724df
LCIMS2 <KursTech@gmail.com>
2025-07-14 13:02:56 +0300

    CSV Intro

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+# CSV-файлы
+import csv
+
+data = [
+    [name,age,city],
+    [Борис,25,Воронеж],
+    [Владимир,28,Тверь],
+    [Глеб,35,Москва]
+]
+
+with open('people.csv','r', encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',', quotchar='"')
+    for row in reader:
+        print(row)
+
+with open('employee.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
 
 
 
