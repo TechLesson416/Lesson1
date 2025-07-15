f0f49b57e9ae7c75700ada354e1a731f6ca92122
LCIMS2 <KursTech@gmail.com>
2025-07-15 15:13:56 +0300

    API Weather

--- main.py
+++ main.py
@@ -1,100 +1,37 @@
-# Базы данных (запись)
-# """
-# # 1. Импорт библиотеки SQL
-# # 2. Подключение к БД
-# # 3. Назначить
-# # 4. Работа с БД (Запросы и ответы)
-# # 5. Отключаемся от БД
-# """
-# import sqlite3
-#
-# # Подключаемся
-# connection = sqlite3.connect('db/movies.sqlite')
-#
-# # Курсор
-# cursor = connection.cursor()
-#
-# # Запрос (с помощью курсора)
-# result = cursor.execute(
-#     """
-#     SELECT title, year FROM films
-#     WHERE year BETWEEN 2001 AND 2005
-#     """
-# )
-#
-# print result.fetchall()
+# Погода через API
+import requests
+from PIL import Image
+import io
 
-# for title, year in array:
-#     print(title, year)
+API_KEY =
+URL =
+CITY =
 
+params = {
 
-import sqlite3
+    'q': CITY,
+    'appid': API_KEY,
+    'units': 'metric',
+    'lang': 'ru'
+}
 
-class Crud:
-    def __init__(self, db_path):
-        self._conn = sqlite3.connect(db_path)
-        self._cur = self._conncursor()
+response = requests.get(URL, params=params)
+result = response.json()
 
-    def create(self, table_name, name, age):
-        self._cur.execute(
-            f"""
-            INSERT INTO users(name, age)
-        VALUES(?,?)
-        """, (name, int(age))
-        )
-        self._conn.commit()
+weather = result['weather'][0]['description']
+temperature = result['main']['temp']
+humidity = result['main']['humidity']
+wind = result['wind']['speed']
+data = result['coords']
+ll = f'{data['lon']},{data['lat']}'
+print(ll)
 
 
-
-
-    def delete(selfself, in_num, table_name):
-            self._cur.execute(
-                f'DELETE FROM {table_name} WHERE id={id_num}'
-            )
-            self._conn.commit()
-
-    def read(self, table_name):
-        res = self._cur.execute(
-             f'SELECT * FROM {table_name}'
-        ).fetchmall()
-        for num, name, age in res:
-             print(id, name, age)
-
-    def update(self, table_name, id_num, name=None, age=None):
-
-        query = f'UPDATE' {table_name} SET name={name}, age={age}, id={id_num}'
-        self._cur.execute(
-            query
-        )
-        self._conn.commit()
-
-
-
-    def delete(selfself, in_num, table_name):
-        self._cur.execute(
-            f'DELETE FROM {table_name} WHERE id={id_num}'
-        )
-        self._conn.commit()
-
-    # method override (переопределяем метод уничтожения объекта)
-    def __del__(self):
-        self._conn.close() # отключаем курсор
-        self._conn.close() # отключаем от БД
-
-db = Crud('db/movies.sqlite')
-#db.delete(3, 'users')
-db.update ('users', 'Евгений', 19)
-db.read('users')
-
-print()
-
-
-
-
-
-
-
-
-
-
-
+print(f'Сегодня в городе {CITY}: {weather}')
+print(f'Температура: {temperature:.1}\xB0'f'C')
+print(f'Влажность: {humidity}')
+print(f'Скорость ветра: {wind}')
+link = https://static-maps.yandex.ru/1.x/?ll={ll}&spn=0.0025,0.0025&l=map&pt={ll},pm2dgl
+image = requests.get(link).content
+if image:
+    Image.open(io.BytesIO(image)).show()
