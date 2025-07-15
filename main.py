60ead6165d247b84f1d8a78b9732333d8083f8d0
LCIMS2 <KursTech@gmail.com>
2025-07-15 16:11:39 +0300

    Decorator Time Measure

--- main.py
+++ main.py
@@ -1,26 +1,72 @@
 # Декораторы
-g = 5
-def outer():
-    x = 5
+import time
 
-    def inner():
-        nonlocal x
-        print('Nonlocal x=', x)
-        x = 10
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        finish = time.time()
+        print(f'Функция исполнялась:{finish - start:.4f} сек.')
+        return result
 
-    inner()
-    print('New x=', x)
+    return wrapper
+@timeit
+def test():
+    time.sleep(0.8)
 
-
-outer()
-
-
-
-
+test()
 
 
 
 
+
+
+
+# def logger(func):
+#     counter = 0
+#     def decorated_func(*args, **kwargs):
+#         nonlocal  counter
+#         counter += 1
+#         print(counter, '->', 'Аргументы:', args,
+#               'Именнованные аргументы:', kwargs)
+#         result = func(*args, **kwargs)
+#         print('____', 'Результат:', result)
+#     return decorated_func
+#
+# @logger
+# def make_burger(meal='говядиной', onion=False, tomato=False):
+#     print('Булочка')
+#     if onion:
+#         print('Луковые кольца')
+#     print('Котлета с', meal)
+#     if tomato:
+#         print('Помидоры')
+#     print('Булочка')
+#
+# make_burger(onion=True)
+#
+# # g = 5
+# def outer():
+#     x = 5
+#
+#     def inner():
+#         nonlocal x
+#         print('Nonlocal x=', x)
+#         x = 10
+#
+#     inner()
+#     print('New x=', x)
+#
+#
+# outer()
+#
+#
+#
+#
+#
+#
+#
+#
 
 # def upper_case_print(old_func):
 #     def new_func(*args, **kwargs):
