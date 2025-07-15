063532faadbf16baa07f74e7b57a9ca350f77023
LCIMS2 <KursTech@gmail.com>
2025-07-15 15:44:28 +0300

    Decorator Intro

--- main.py
+++ main.py
@@ -1,37 +1,74 @@
+# Декораторы
+def upper_case_print(old_func):
+    def new_func(*args, **kwargs):
+        args_up_case = [str(arg).upper() for arg in args]
+        old_func(*args_up_case, **kwargs)
+    return new_func
+
+new_print = upper_case_print(print)
+new_print('Привет, Пока')
+
+
+# def answer(question):
+#     return 'думайте сами'
+#
+#
+# def dialog():
+#     def answer(question):
+#         if question.lower().startswith('когда'):
+#             return 'Никогда'
+#         else:
+#             return 'Упппс'
+#     question = input()
+#     while question != '':
+#         print(answer(question))
+#         question = input()
+#
+# dialog()
+
+
+
+
+
+
+
+
+
 # Погода через API
-import requests
-from PIL import Image
-import io
+# import requests
+# from PIL import Image
+# import io
+#
+# API_KEY =
+# URL =
+# CITY =
+#
+# params = {
+#
+#     'q': CITY,
+#     'appid': API_KEY,
+#     'units': 'metric',
+#     'lang': 'ru'
+# }
+#
+# response = requests.get(URL, params=params)
+# result = response.json()
+#
+# weather = result['weather'][0]['description']
+# temperature = result['main']['temp']
+# humidity = result['main']['humidity']
+# wind = result['wind']['speed']
+# data = result['coords']
+# ll = f'{data['lon']},{data['lat']}'
+# print(ll)
+#
+#
+# print(f'Сегодня в городе {CITY}: {weather}')
+# print(f'Температура: {temperature:.1}\xB0'f'C')
+# print(f'Влажность: {humidity}')
+# print(f'Скорость ветра: {wind}')
+# link = https://static-maps.yandex.ru/1.x/?ll={ll}&spn=0.0025,0.0025&l=map&pt={ll},pm2dgl
+# image = requests.get(link).content
+# if image:
+#     Image.open(io.BytesIO(image)).show()
 
-API_KEY =
-URL =
-CITY =
-
-params = {
-
-    'q': CITY,
-    'appid': API_KEY,
-    'units': 'metric',
-    'lang': 'ru'
-}
-
-response = requests.get(URL, params=params)
-result = response.json()
-
-weather = result['weather'][0]['description']
-temperature = result['main']['temp']
-humidity = result['main']['humidity']
-wind = result['wind']['speed']
-data = result['coords']
-ll = f'{data['lon']},{data['lat']}'
-print(ll)
-
-
-print(f'Сегодня в городе {CITY}: {weather}')
-print(f'Температура: {temperature:.1}\xB0'f'C')
-print(f'Влажность: {humidity}')
-print(f'Скорость ветра: {wind}')
-link = https://static-maps.yandex.ru/1.x/?ll={ll}&spn=0.0025,0.0025&l=map&pt={ll},pm2dgl
-image = requests.get(link).content
-if image:
-    Image.open(io.BytesIO(image)).show()
