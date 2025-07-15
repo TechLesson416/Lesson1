e99c966032605724bedd1008063c3675660063ae
LCIMS2 <KursTech@gmail.com>
2025-07-15 14:35:51 +0300

    SQL Class CRUD

--- main.py
+++ main.py
@@ -1,103 +1,92 @@
-# JSON - (Java Script Object Notation)
-# Для чтения:
-# load() - читает из файла
-# loads() - читает строковое представление
-import json
-
-with open('dogs.json', 'rt') as d:
-    # data = json.load(d) # Напрямую из файла
-    temp = d.read()  #Читаем файл как строку
-    data = json.loads(temp) # строковое представление JSON
-
-for k, v in data.items():
-    if type(v) == list:
-        print(f'{k}: {', '.join(v)})
-    else:
-        print(f'{k}: {v}')
-
-
-
-
-# Zip
-# from zipfile import ZipFile
-# import os
-
-# csv_files = [f for f in os.listdir() if f.endswith('.csv')]
-# #print(csv_files)
-# with ZipFile('archive.zip', 'w') as myzip:
-#     for file in vsc_files:
-#         myzip.write(file)
-#         os.remove(file)
-
-
-
-files_to_extract = ['people.csv', 'file.csv']
-
-# Распаковать
-
-
-# with ZipFile('archive.zip', 'r') as zip_obj:
-#     zip_obj.extractall(members = files_to_extract)
-
-
-# Получить список
-with ZipFile('archive.zip', 'r') as zip_obj:
-    print(zip_obj.nameList())
-
-
-
-
-# CSV-файлы
-# import csv
-
-
-
-
-
-# with open('people.csv','r', encoding='utf-8') as f:
-#     dict_reader = csv.DictReader(f)
-#     for row in dict_reader:
-#         print(f'{row'['name']} живёт в городе {row['city']}')
-            
-# field_names = ['name', 'age', 'city']
-# data = {
-#     'name':'Борис'
-#     'age': 27,
-#     'city': 'Москва'
-# }
+# Базы данных (запись)
+# """
+# # 1. Импорт библиотеки SQL
+# # 2. Подключение к БД
+# # 3. Назначить
+# # 4. Работа с БД (Запросы и ответы)
+# # 5. Отключаемся от БД
+# """
+# import sqlite3
 #
-# with open('file.csv', 'w', newline = '', encoding='utf-8') as f:
-#     writer = csv.DictWriter(f, fieldnames=field_names)
-#     writer.writerow(data)
-
-# Режимы квотирования
-
-data = [ 'name', 25, 'town']
-with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    writer.writerow(data)
-
-
-#data = [
-#    ['name','age','city'],
-#   ['Борис','25','Воронеж'],
-#    ['Владимир','28','Тверь'],
-#    ['Глеб','35','Москва']
-#]
-
-
-
-
-# with open('people.csv','r', encoding='utf-8') as f:
-#     reader = csv.reader(f, delimiter=',', quotchar='"')
-#     for row in reader:
-#         print(row)
+# # Подключаемся
+# connection = sqlite3.connect('db/movies.sqlite')
 #
-# with open('employee.csv', 'w', newline='', encoding='utf-8') as f:
-#     writer = csv.writer(f)
-#     writer.writerows(data)
+# # Курсор
+# cursor = connection.cursor()
+#
+# # Запрос (с помощью курсора)
+# result = cursor.execute(
+#     """
+#     SELECT title, year FROM films
+#     WHERE year BETWEEN 2001 AND 2005
+#     """
+# )
+#
+# print result.fetchall()
+
+# for title, year in array:
+#     print(title, year)
 
 
+import sqlite3
+
+class Crud:
+    def __init__(self, db_path):
+        self._conn = sqlite3.connect(db_path)
+        self._cur = self._conncursor()
+
+    def create(self, table_name, name, age):
+        self._cur.execute(
+            f"""
+            INSERT INTO users(name, age)
+        VALUES(?,?)
+        """, (name, int(age))
+        )
+        self._conn.commit()
+
+
+
+
+    def delete(selfself, in_num, table_name):
+            self._cur.execute(
+                f'DELETE FROM {table_name} WHERE id={id_num}'
+            )
+            self._conn.commit()
+
+    def read(self, table_name):
+        res = self._cur.execute(
+             f'SELECT * FROM {table_name}'
+        ).fetchmall()
+        for num, name, age in res:
+             print(id, name, age)
+
+    def update(self, table_name, id_num, name=None, age=None):
+
+        query = f'UPDATE' {table_name} SET name={name}, age={age}, id={id_num}'
+        self._cur.execute(
+            query
+        )
+        self._conn.commit()
+
+
+
+    def delete(selfself, in_num, table_name):
+        self._cur.execute(
+            f'DELETE FROM {table_name} WHERE id={id_num}'
+        )
+        self._conn.commit()
+
+    # method override (переопределяем метод уничтожения объекта)
+    def __del__(self):
+        self._conn.close() # отключаем курсор
+        self._conn.close() # отключаем от БД
+
+db = Crud('db/movies.sqlite')
+#db.delete(3, 'users')
+db.update ('users', 'Евгений', 19)
+db.read('users')
+
+print()
 
 
 
