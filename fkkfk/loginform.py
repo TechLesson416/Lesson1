cf9f2ab3fe18d1df4b1ef796bf11ec662688993c
LCIMS2 <KursTech@gmail.com>
2025-07-17 15:29:56 +0300

    ORM Continue

---
+++ fkkfk/loginform.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordFiels
+from wtforms import BoolenField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired('Это обязательное поле')])
+    password = PasswordField('Пароль', validators=[DataRequired('Без пароля нельзя!')])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
